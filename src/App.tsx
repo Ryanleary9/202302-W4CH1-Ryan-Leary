82b24820fb002ba9956736a7146966bcde28d773
Ryan <ryanlearyzafra@gmail.com>
2023-02-07 00:48:45 +0100

    Fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Info } from "./components/info/info";
 import { Gentleman } from "./components/gentleman/gentleman";
